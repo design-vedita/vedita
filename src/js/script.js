16818db5c4d47f6a5961bb1ae454737003113ab1
nikalun <emptyfull@rambler.ru>
2016-01-22 09:22:35 +0300

    index page menu bird click

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
 
     $(function(){
 
+        /*-----------------preloader--------------------*/
         $(window).on('load', function () {
             var $preloader = $('.page-preloader'),
                 $spinner   = $preloader.find('.spinner');
@@ -10,6 +11,7 @@
             $preloader.addClass('slow');
         });
 
+        /*-----------------visible menu----------------*/
         var burger = document.getElementsByClassName('js-transform')[0],
             menu = document.getElementsByClassName('js-menu')[0],
             menu_div = menu.getElementsByTagName('div');
@@ -23,10 +25,23 @@
                 menu_div[1].classList.toggle('menu-show');
             }
 
+        /*----------------slider index---------------*/
         var swiper = new Swiper('.swiper-container', {
             pagination: '.swiper-pagination',
             paginationClickable: true,
             direction: 'vertical'
+        });
+
+        /*--------------click on bird in hide-menu----------*/
+        $('.js-arrow-menu').on('click', function(){
+            var $parent = $(this).parent(),
+                $menu_hide = $('.js-hide-menu li');
+
+                $($menu_hide).each(function(){
+                    $(this).removeClass('show-before');
+                });
+
+                $parent.addClass('show-before');
         });
 
     });
